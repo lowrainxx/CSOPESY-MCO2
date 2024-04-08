bfea750ba52eb3621f5c99411a85be23e32bb1b0
lowrain <lorane_bernadeth_fausto@dlsu.edu.ph>
2024-04-08 13:29:13 +0800

    UPDATE: logic lol

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import threading
+import os
 
 super_citizen_semaphore = threading.Semaphore(2) # at least 1, at most 2
 regular_citizen_semaphore = threading.Semaphore(3) # at least 2, at most 3
@@ -9,29 +10,31 @@ super_citizens_waiting = 0
 regular_citizens_waiting = 0
 
 def super_citizen():
-    pass
+    print("Super Citizen signing up\n")
 
 def regular_citizen():
-    pass
+    print("Regular Citizen signing up\n")
 
 # Signup queue
-def signups():
+def simulate_signup():
     threads = []
-    for _ in range(s):
-        t = threading.Thread(target=super_citizen)
-        threads.append(t)
-        t.start()
 
-    for _ in range(r):
-        t = threading.Thread(target=regular_citizen)
-        threads.append(t)
-        t.start()
-
-    for t in threads:
-        t.join()
+    t1 = threading.Thread(target=super_citizen, name='t1')
+    t2 = threading.Thread(target=regular_citizen, name='t2')
+ 
+    t1.start()
+    t2.start()
+ 
+    t1.join()
+    t2.join()
 
 global r, s
 r = input("Input the total number of regular citizens: ")
 s = input("Input the total number of super citizens: ")
 
-signups()
+if(r > 0 and s >= 0):
+      simulate_signup()
+
+print("\n----------DONE----------\n")
+
+# print("ID of process running main program: {}".format(os.getpid()))
