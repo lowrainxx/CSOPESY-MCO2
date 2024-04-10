8649c182b3526964c76d2f621ec366b60958b996
lowrain <lorane_bernadeth_fausto@dlsu.edu.ph>
2024-04-10 20:44:18 +0800

    FINISHED (ver.1)
    has r+s+1 number of threads

--- main.py
+++ main.py
@@ -1,40 +1,142 @@
-import threading
-import os
+from threading import Thread, Semaphore, Lock
+import random 
+import time
 
-super_citizen_semaphore = threading.Semaphore(2) # at least 1, at most 2
-regular_citizen_semaphore = threading.Semaphore(3) # at least 2, at most 3
-team_lock = threading.Lock()
+'''
+TERMS:
+threading.Thread() = returns thread object
+    daemon = True if thread should exit when program exits
+                  else False
+Thread.start() =
+Thread.join() =
+threading.Semaphore() = to limit number of threads
+Semaphore.acquire() = acquire semaphore
+Semaphore.release() = release semaphore
+    blocking = False if thread will not block if semaphore isnt available
+                     else True
+threading.Lock() =
+'''
 
-# Waiting queue counter
-super_citizens_waiting = 0
-regular_citizens_waiting = 0
+class Helldivers:
+    def __init__(self, r, s):
+        self.r = r
+        self.s = s 
+        self.team_id = 0
+        self.sc_id = 0
+        self.rc_id = 0
+        self.regular_queue = Semaphore(1)
+        self.super_queue = Semaphore(1)
+        self.lock = Lock()
+        self.total_teams_sent = 0
+        self.unsent_rc = r
+        self.unsent_sc = s
 
-def super_citizen():
-    print("Super Citizen signing up\n")
+    def regular_citizen(self):
+        time.sleep(random.uniform(0.1, 0.5))  # Simulate queueing time
+        self.regular_queue.release()
 
-def regular_citizen():
-    print("Regular Citizen signing up\n")
+    def super_citizen(self):
+        time.sleep(random.uniform(0.1, 0.5))  # Simulate queueing time
+        self.super_queue.release()
+    
+    def assemble_team(self):
+            while True:
+                # Check if there are enough citizens to form a new team
+                if self.r==0 or self.s==0 or (self.r + self.s < 4) or self.r<2 or (self.r>2 and self.s<2):
+                    if self.r>2 and self.s==1: pass
+                    else: # Not enough citizens to form another team
+                        for _ in range(self.r):
+                            self.rc_id+=1
+                            self.r-=1
+                            print(f'Regular Citizen {self.rc_id} is signing up')
+                        for _ in range(self.s):
+                            self.sc_id+=1
+                            self.s-=1
+                            print(f'Super Citizen {self.sc_id} is signing up')
+                        break
+                
+                # Need at least 1 Super Citizen
+                self.super_queue.acquire()  
+                with self.lock:
+                    self.s -= 1
+                    super_count = 1
+                    self.team_id += 1
+                    self.sc_id += 1
+                    team_id = self.team_id
+                    print(f'Super Citizen {self.sc_id} is signing up')
 
-# Signup queue
-def simulate_signup():
-    threads = []
+                # Attempt to add a second Super Citizen
+                if (self.r==2 and self.s>0) or self.super_queue.acquire(blocking=False):
+                    with self.lock:
+                        if self.s > 0:
+                            self.s -= 1
+                            self.sc_id += 1
+                            super_count += 1
+                            print(f'Super Citizen {self.sc_id} is signing up')
+                        else:
+                            self.regular_queue.release()  # Release if not used
 
-    t1 = threading.Thread(target=super_citizen, name='t1')
-    t2 = threading.Thread(target=regular_citizen, name='t2')
- 
-    t1.start()
-    t2.start()
- 
-    t1.join()
-    t2.join()
+                # Regular Citizens Sign-up
+                regular_needed = 4 - super_count
+                for _ in range(regular_needed):
+                    self.regular_queue.acquire()
+                    with self.lock:
+                        self.r -= 1
+                        self.rc_id += 1
+                        print(f'Regular Citizen {self.rc_id} is signing up')
+                        self.regular_queue.release()
 
-global r, s
-r = input("Input the total number of regular citizens: ")
-s = input("Input the total number of super citizens: ")
+                # Joining and sending team        
+                with self.lock:
+                    self.total_teams_sent += 1
+                    self.unsent_rc = self.r
+                    self.unsent_sc = self.s
 
-if(r > 0 and s >= 0):
-      simulate_signup()
+                    temp_id = self.sc_id - super_count
+                    for _ in range (super_count):
+                        temp_id+=1
+                        print(f'Super Citizen {temp_id} has joined team {team_id}')
 
-print("\n----------DONE----------\n")
+                    temp_id = self.rc_id - regular_needed
+                    for _ in range (regular_needed):
+                        temp_id+=1
+                        print(f'Regular Citizen {temp_id} has joined team {team_id}')
+                    print(f'team {team_id} is ready and now launching to battle (sc: {super_count} | rc: {4 - super_count})\n')
 
-# print("ID of process running main program: {}".format(os.getpid()))
+    def run(self):
+        # Citizen queueing simulation
+        threads = []
+        for _ in range(self.r):
+            t = Thread(target=self.regular_citizen, daemon=True)
+            t.start()
+            threads.append(t)
+
+        for _ in range(self.s):
+            t = Thread(target=self.super_citizen, daemon=True)
+            t.start()
+            threads.append(t)
+
+        assemble_team = Thread(target=self.assemble_team)
+        assemble_team.start()
+        threads.append(assemble_team)
+
+        for t in threads:
+            t.join()
+
+        print(f"\nTotal teams sent: {self.total_teams_sent}")
+        print(f"Unsent Regular Citizens: {self.unsent_rc}\nUnsent Super Citizens: {self.unsent_sc}")
+
+if __name__ == "__main__":
+    #UNCOMMENT FOR SUBMISSION
+    # r = int(input("Input the total number of regular citizens: "))
+    # s = int(input("Input the total number of super citizens: "))
+
+    # if(r >= 0 and s >= 0):
+    #     print("\n")
+    #     hq = Helldivers(r, s)
+    #     hq.run()
+    
+    hq = Helldivers(32, 12) 
+    hq.run()
+
+    print("\n-----------------END-----------------\n")
