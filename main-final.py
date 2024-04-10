49c33010ebe390ef0bfea397f3afd67597968a0a
lowrain <lorane_bernadeth_fausto@dlsu.edu.ph>
2024-04-10 21:09:53 +0800

    UPDATE: documentation

--- main-final.py
+++ main-final.py
@@ -14,51 +14,63 @@ Semaphore.acquire() = acquire semaphore
 Semaphore.release() = release semaphore
     blocking = False if thread will not block if semaphore isnt available
                      else True
-threading.Lock() =
+threading.Lock() = mutex lock
+'''
+
+'''
+SYNCHRONIZATION TECHNIQUE USED: SEMAPHORES
 '''
 
 class Helldivers:
     def __init__(self, r, s):
-        self.r = r
-        self.s = s 
-        self.team_id = 0
-        self.sc_id = 0
-        self.rc_id = 0
-        self.regular_queue = Semaphore(1)
-        self.super_queue = Semaphore(1)
-        self.lock = Lock()
-        self.total_teams_sent = 0
-        self.unsent_rc = r
-        self.unsent_sc = s
+        self.r = r                  # Number of Regular Citizens
+        self.s = s                  # Number of Super Citizens
+        self.team_id = 0            # Team ID
+        self.rc_id = 0              # Regular Citizens ID
+        self.sc_id = 0              # Super Citizens ID
+        self.regular_queue = Semaphore(1)   # Semaphore for Regular Citizens (Limit: 1)
+        self.super_queue = Semaphore(1)     # Semaphore for Super Citizens (Limit: 1)
+        self.lock = Lock()          # Mutex Lock
+        self.total_teams_sent = 0   # Total Teams Sent
+        self.unsent_rc = r          # Unsent Regular Citizens
+        self.unsent_sc = s          # Unsent Super Citizens
 
     def regular_citizen(self):
+        # Queue for Regular Citizens
         for _ in range(self.r):
-            time.sleep(random.uniform(0.1, 0.5))  # Simulate queueing time
+            time.sleep(random.uniform(0.1, 0.5))  # to simulate queueing time
             self.regular_queue.release()
 
     def super_citizen(self):
+        # Queue for Super Citizens
         for _ in range(self.s):
-            time.sleep(random.uniform(0.1, 0.5))  # Simulate queueing time
+            time.sleep(random.uniform(0.1, 0.5))  # to simulate queueing time
             self.super_queue.release()
     
     def assemble_team(self):
             while True:
                 # Check if there are enough citizens to form a new team
+                ''' SATISFIES RULE #7 ''' 
                 if self.r==0 or self.s==0 or (self.r + self.s < 4) or self.r<2 or (self.r>2 and self.s<2):
-                    if self.r>2 and self.s==1: pass
-                    else: # Not enough citizens to form another team
+                    if self.r>2 and self.s==1: pass # There is still 1 SC left and more than 2 RCs
+                    else: # Not enough citizens to form another team 
+                        # Simulate remaining Regular Citizen signing up
                         for _ in range(self.r):
                             self.rc_id+=1
                             self.r-=1
                             print(f'Regular Citizen {self.rc_id} is signing up')
+                        # Simulate remaining Super Citizen signing up
                         for _ in range(self.s):
                             self.sc_id+=1
                             self.s-=1
                             print(f'Super Citizen {self.sc_id} is signing up')
                         break
                 
-                # Need at least 1 Super Citizen
-                self.super_queue.acquire()  
+                # TEAM ASSEMBLY #
+
+                # Super Citizen Sign up (Need at least 1)
+                ''' SATISFIES RULE #2 '''
+                self.super_queue.acquire()
                 with self.lock:
                     self.s -= 1
                     super_count = 1
@@ -67,7 +79,8 @@ class Helldivers:
                     team_id = self.team_id
                     print(f'Super Citizen {self.sc_id} is signing up')
 
-                # Attempt to add a second Super Citizen
+                ''' SATISFIES RULE #2 & #3 '''
+                # Attempt to add 2nd Super Citizen
                 if (self.r==2 and self.s>0) or self.super_queue.acquire(blocking=False):
                     with self.lock:
                         if self.s > 0:
@@ -76,10 +89,12 @@ class Helldivers:
                             super_count += 1
                             print(f'Super Citizen {self.sc_id} is signing up')
                         else:
-                            self.regular_queue.release()  # Release if not used
+                            print("super_queue released\n")
+                            self.super_queue.release()  # Release if not used
 
-                # Regular Citizens Sign-up
-                regular_needed = 4 - super_count
+                # Regular Citizen Sign up
+                ''' SATISFIES RULE #1 & #4 '''
+                regular_needed = 4 - super_count # Regular Citizens fill up team
                 for _ in range(regular_needed):
                     self.regular_queue.acquire()
                     with self.lock:
@@ -88,7 +103,8 @@ class Helldivers:
                         print(f'Regular Citizen {self.rc_id} is signing up')
                         self.regular_queue.release()
 
-                # Joining and sending team        
+                # Joining and sending team       
+                ''' SATISFIES RULE #6''' 
                 with self.lock:
                     self.total_teams_sent += 1
                     self.unsent_rc = self.r
@@ -109,24 +125,23 @@ class Helldivers:
         # Citizen queueing simulation
         threads = []
 
-        # Start a single thread for all regular citizens
+        # Thread for Regular Citizens
         regular_citizens_thread = Thread(target=self.regular_citizen, daemon=True)
         regular_citizens_thread.start()
         threads.append(regular_citizens_thread)
 
-        # Start a single thread for all super citizen
+        # Thread for Super Citizens
         super_citizen_thread = Thread(target=self.super_citizen, daemon=True)
         super_citizen_thread.start()
         threads.append(super_citizen_thread)
 
-        # Start a single thread for assembling team
+        # Thread for Team Assembly
         assemble_team_thread = Thread(target=self.assemble_team)
         assemble_team_thread.start()
         threads.append(assemble_team_thread)
 
-        # Wait for all threads to complete
+        # Joining of threads
         for t in threads:
-            print(f"Joining")
             t.join()
 
         print(f"\nTotal teams sent: {self.total_teams_sent}")
@@ -134,15 +149,17 @@ class Helldivers:
 
 if __name__ == "__main__":
     #UNCOMMENT FOR SUBMISSION
+    # # Ask for user input r, s
     # r = int(input("Input the total number of regular citizens: "))
     # s = int(input("Input the total number of super citizens: "))
 
+    # # Checks if r & s are not negative
     # if(r >= 0 and s >= 0):
     #     print("\n")
     #     hq = Helldivers(r, s)
     #     hq.run()
     
-    hq = Helldivers(32, 12) 
+    hq = Helldivers(3, 1) 
     hq.run()
 
     print("\n-----------------END-----------------\n")
